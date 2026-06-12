fc8af2e6a6242844498e3eaa6dbb85e178ac2873
jugstalt <gstalt@gmx.at>
2026-06-12 09:25:36 +0200

    archive

--- src/Proj4Net.Playground/Program.cs
+++ src/Proj4Net.Playground/Program.cs
@@ -26,14 +26,14 @@ ProjCoordinate p0 = new ProjCoordinate();
 //p.Y = 318802.50516882259;
 
 // 15 48 with grid & ellips
-var fromCrs = crsFactory.CreateFromParameters(to, "+proj=tmerc +lat_0=0 +lon_0=16.33333333333333 +k=1.000000 +x_0=0 +y_0=-5000000 +ellps=bessel +units=m +nadgrids=AT_GIS_GRID_2021_09_28.gsb,ellps:GRS80");
-p.X = -99411.663305323091;
-p.Y = 318802.50516882259;
+//var fromCrs = crsFactory.CreateFromParameters(to, "+proj=tmerc +lat_0=0 +lon_0=16.33333333333333 +k=1.000000 +x_0=0 +y_0=-5000000 +ellps=bessel +units=m +nadgrids=AT_GIS_GRID_2021_09_28.gsb,ellps:GRS80");
+//p.X = -99411.663305323091;
+//p.Y = 318802.50516882259;
 
 // 15 48 with +towgs84
-//var fromCrs = crsFactory.CreateFromParameters(to, "+proj=tmerc +lat_0=0 +lon_0=16.33333333333333 +k=1.000000 +x_0=0 +y_0=-5000000 +ellps=bessel +units=m +towgs84=577.326,90.129,463.919,5.137,1.474,5.297,2.4232");
-//p.X = -99411.687267207672;
-//p.Y = 318802.40399568342;
+var fromCrs = crsFactory.CreateFromParameters(to, "+proj=tmerc +lat_0=0 +lon_0=16.33333333333333 +k=1.000000 +x_0=0 +y_0=-5000000 +ellps=bessel +units=m +towgs84=577.326,90.129,463.919,5.137,1.474,5.297,2.4232");
+p.X = -99411.687267207672;
+p.Y = 318802.40399568342;
 
 
 // 15 48 without datum
@@ -50,7 +50,7 @@ ICoordinateTransform trans = ctFactory.CreateTransform(fromCrs, toCrs);
 
 
 //Algorithms.DefaultGeocentricToGeodeticAlgorithm = GeocentricToGeodeticAlgorithm.Vermeille;
-int interations = 1; // 10_000_000;
+int interations = 10_000_000;
 
 var helper = new Coordinate();
 // Warmup
